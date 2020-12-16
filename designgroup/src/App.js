131f0b228d3fc51ef54df44b37ce6acefa05235c
Zeriab <zeriab@hotmail.com>
2020-12-16 12:22:04 +0200

    menu

--- designgroup/src/App.js
+++ designgroup/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import Layout from "./Layout/Layout"
+import Sidebar from './Layout/Sidebar';
+
+// React Notification
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" id="outer-container">
+       <Sidebar  pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+    <div id="page-wrap">
+      
+   <Layout />
+   </div>
+   </div>
   );
 }
 
