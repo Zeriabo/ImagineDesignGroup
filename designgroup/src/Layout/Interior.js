7bfa56a07783b67625a9e788b304ca04177e2239
Zeriab <zeriab@hotmail.com>
2020-12-23 20:47:50 +0200

    Interior form

--- designgroup/src/Layout/Interior.js
+++ designgroup/src/Layout/Interior.js
@@ -1,113 +1,188 @@
 import React, { useState, useCallback, Component } from 'react'
-import { render } from 'react-dom'
+import  {useTransition}  from 'react-spring'
 import {  animated } from 'react-spring'
 import { SketchPicker } from 'react-color'
 import img1 from './images/i1.jpeg'
 import img2 from './images/i2.jpeg'
 import img3 from './images/i3.jpeg'
+import _default from 'react-bootstrap/esm/ModalFooter'
 //import transitions from './trans'
-
-const images = [img1,img2,img3]
-var name,theme;
-var colors=['#C0FFEE','#4A90E2']
-var page = [
-  
-  ({ style }) => <animated.div style={{ ...style}}>
-  
-  <form>
-<table border = '1'>
-    <tr><td><br /></td></tr>
-    <tr><td><br /></td></tr>
-    <tr><td><br /></td></tr>
-<tr><td>
-     <label>
-    Name:
-    </label>
-    </td>
-    <td>
-    <input type="text" name="name" value={name} />
-   </td></tr>
-<tr><td> <label>
+export default class Interior extends Component { 
+  state = {color: '#fff',index:0,colors:[]};
+ 
      
-     Theme:
+   handleSubmit = this.handleSubmit.bind(this);
+
+   handleChangeConfirmed = (color) => {
+     if(this.state.colors.length!=5){
+    this.setState({ colors:[...this.state.colors,this.state.color]});
+    
+  }else{
+    alert("Your cant add more than "+this.state.colors.length+" Colors")
+  }
+   }
+   handleRemoveLast = ()=>{
+   console.log(this.state.colors.length)
+      if(this.state.colors.length==1)  { document.getElementById("myDIV1").style.backgroundColor = '#FFFAF0'
+      document.getElementById("myDIV2").style.backgroundColor = '#FFFAF0'
+      document.getElementById("myDIV3").style.backgroundColor = '#FFFAF0'
+      document.getElementById("myDIV4").style.backgroundColor = '#FFFAF0'
+      document.getElementById("myDIV5").style.backgroundColor = '#FFFAF0'}
+    else if (this.state.colors.length==2)  {document.getElementById("myDIV2").style.backgroundColor = '#FFFAF0'
+    document.getElementById("myDIV3").style.backgroundColor = '#FFFAF0'
+    document.getElementById("myDIV4").style.backgroundColor = '#FFFAF0'
+    document.getElementById("myDIV5").style.backgroundColor = '#FFFAF0'
+  }
+    else if   (this.state.colors.length==3)  { document.getElementById("myDIV3").style.backgroundColor = '#FFFAF0'
+    document.getElementById("myDIV4").style.backgroundColor = '#FFFAF0'
+    document.getElementById("myDIV5").style.backgroundColor = '#FFFAF0'}
+    else if(this.state.colors.length==4) { document.getElementById("myDIV4").style.backgroundColor = '#FFFAF0'
+    document.getElementById("myDIV5").style.backgroundColor = '#FFFAF0'}
+    else if (this.state.colors.length==5) document.getElementById("myDIV5").style.backgroundColor = '#FFFAF0'
+
+     if(this.state.colors.length>0)
+     {
+       this.state.colors.pop();
+
+       console.log(this.state.colors)
+     }else{
+       console.log("Can't Remove from Empty list")
+     }
+   }
+
+   handleEmptyList = ()=>{
+    if(this.state.colors.length>0)
+    {
+      this.state.colors=[]
+        document.getElementById("myDIV1").style.backgroundColor ='#FFFAF0'
+        document.getElementById("myDIV2").style.backgroundColor = '#FFFAF0'
+     document.getElementById("myDIV3").style.backgroundColor = '#FFFAF0'
+       document.getElementById("myDIV4").style.backgroundColor = '#FFFAF0'
+        document.getElementById("myDIV5").style.backgroundColor = '#FFFAF0'
+      console.log(this.state.colors)
+    }else{
+      console.log("Can't Empty list")
+    }
+  }
+  
+ handleChangeComplete = (color) => { 
+
+  this.setState({ color: color.hex });
+
+  return (this.state.colors.length==0)?   document.getElementById("myDIV1").style.backgroundColor = color.hex:
+  (this.state.colors.length==1)?   document.getElementById("myDIV2").style.backgroundColor = color.hex:
+  (this.state.colors.length==2)?   document.getElementById("myDIV3").style.backgroundColor = color.hex:
+  (this.state.colors.length==3)?   document.getElementById("myDIV4").style.backgroundColor = color.hex:
+  (this.state.colors.length==4)?   document.getElementById("myDIV5").style.backgroundColor = color.hex:null
+ };
+  handleSubmit(e) {
+    alert('The value is: ' + this.state.colors);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+       <table border = '1' >
+    <tr><td><br /></td></tr>
+    <tr><td><br /></td></tr>
+    <tr><td><br /></td></tr>
+ <tr><td>
+      <label>
+     Name:
      </label>
      </td>
-     <td>
-     <select name="cars" id="cars" size="10">
-     <option value="volvo">Chinese</option>
-     <option value="saab">Egyptian</option>
-     <option value="opel">Siamese</option>
+    <td>
+    <input type="text" name="name"  />
+    </td></tr>
+ <tr><td> <label>
+     
+      Theme:
+      </label>
+     </td>
+      <td>
+      <select name="theme" id="cars" size="10">
+     <option value="Chinese">Chinese</option>
+     <option value="Egyptian">Egyptian</option>
+      <option value="Siamese">Siamese</option>
+      <option value="Korean">Korean</option>
+     <option value="Korean">Korean</option>
      <option value="audi">Korean</option>
-     <option value="audi">Korean</option>
-     <option value="audi">Korean</option>
-     <option value="audi">Korean</option>
-     <option value="audi">Korean</option>
-     <option value="audi">Korean</option>
-     <option value="audi">Korean</option>
-   </select></td></tr>
+    <option value="audi">Korean</option>
+      <option value="audi">Korean</option>
+      <option value="audi">Korean</option>
+      <option value="audi">Korean</option>
+    </select></td></tr>
  
-</table>
+ </table>
  
-,
-<tr>
-   <td> <label>
+
+ <tr>
+    <td> <label>
      
      Color:
      </label>
-     </td><td><SketchPicker 
- width={220}
+      </td><td><SketchPicker 
+                 width={220}
+                 color={this.state.color}
+                 onChange={this.handleChangeComplete}
+      /></td>   
+      <td></td>
+     
+       <td><label>Color 1</label>
+         <div class="myDIV" id="myDIV1">
 
- color={Interior.color}
-                    onChange={Interior.handleChangeComplete}
+</div></td><td><label>Color 2</label>
+  <div class="myDIV" id="myDIV2" >
 
-     /></td>
-   </tr>
-   <tr><td>
-     <SketchPicker  /></td></tr>
-   <input type="submit" value="Submit" /> 
- 
-</form>
+</div></td>
+<td><label>Color 3</label><div class="myDIV" id="myDIV3" >
 
+</div></td>
+<td><label>Color 4</label><div class="myDIV" id="myDIV4" >
 
-  </animated.div>,
+</div></td>
+<td><label>Color 5</label><div class="myDIV" id="myDIV5" >
 
- ]
- 
+</div></td>
+       </tr>
+<tr> <td><button  type='button' action='' onClick ={this.handleChangeConfirmed}>Confirm Color</button></td>
+       <td><button  type='button' action='' onClick ={this.handleRemoveLast}>Remove last</button></td>
+       <td><button  type='button' action='' onClick ={this.handleEmptyList}>Empty list</button></td></tr>
 
-  
- export class Interior extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = { selection:[],colors:[5],color:"" ,index:0};
-    
-
-
-}
-
-    
-    handleChangeComplete = (color, event) => {
-      this.setState({ color:[...colors,color.hex ]});
-    };
-    
-  
-    
-    
-  
-    render() { 
-    return (
-    
+       <tr><td>Files:</td>
+       <label for="myfile">Select a file:</label>
+       <div> 
+                <input type="file" onChange={this.onFileChange} /> 
+                <button onClick={this.onFileUpload}> 
+                  Upload! 
+                </button> 
+            </div> 
+       <td><input type="file" id="myfile" name="myfile"> </input> </td>
        
-        transitions.map(({ item, props, key }) => {
-          const Page = page[item]
-          return <Page key={key} style={props}>  
+       
+       
+       </tr>
 
-         </Page>
-        })
-        
-   
-    )
+    <input type="submit" value="Submit" /> 
+ 
+      </form>
+    );
   }
 }
-export default Interior
+
+
+
+ 
+
+
+
+
+  
+
+ 
+   
+    
+
+
   
